3b08d8ac7b7c895b524b52af4e0c5aa688827cd0
Rafael Ramos Machado <rafa.ramosmachado@gmail.com>
2021-12-26 22:14:35 -0300

    Update attento.sql

--- attento.sql
+++ attento.sql
@@ -1,5 +1,4 @@
 create database dbagenda_atento;
-show databases;
 use dbagenda_atento;
 create table atendimentos(
 	id_atendimento int primary key auto_increment,
@@ -14,24 +13,4 @@ create table atendimentos(
     data_cirurgia date not null,
     horario_cirurgia varchar(50) not null
 );
-show tables;
-describe atendimentos;
 
-drop table atendimentos;
-
-/* CRUD CREATE */
-insert into atendimentos(nome_cliente,telefone_cliente,fisio,local_hospital,
-nome_cirurgiao,procedimento_cirurgia,cor_tape,data_avaliacao,data_cirurgia,horario_cirurgia) values('Teste',
-'33334444','Juliana','hospital','jose','lipo','azul','2021-10-10','2021-10-23','16:20');
-
-/* CRUD READ */
-select * from atendimentos; 
-select * from atendimentos order by data_avaliacao;
-
-select * from atendimentos where id_atendimento=1;
-
-select nome_cliente,data_avaliacao,data_cirurgia, to_char(data_cirurgia, 'DD/MM/YYYY') from atendimentos;
-
-DELETE FROM atendimentos WHERE id_atendimento = 2;
-
-/* CRUD UPDATE */
